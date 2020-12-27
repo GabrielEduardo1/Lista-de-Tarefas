7c2096f9d8436520ed38774e45a26d925ed44db2
Gabriel Eduardo de Souza <bieleprogramador22@gmail.com>
2020-12-27 14:32:21 -0300

    Parte 1 - Header do Aplicativo

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
 
 export default function App() {
+
+  const image = require('./images/bg.jpg')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} style={styles.image}>
+        <View style={styles.coverView}>
+          <Text style={styles.textHeader}>LISTA DE TAREFAS</Text>
+        </View> 
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +21,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    width:'100%',
+    height:100,
+  },
+  coverView:{
+    width:'100%',
+    height:100,
+    backgroundColor:'rgba(0,0,0,0.6)'
+  },
+  textHeader:{
+    color:'white',
+    fontSize:20,
+    textAlign:'center',
+    marginTop:30
+  }
 });
