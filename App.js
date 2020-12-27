db538985af9c06c68e701c2726ed342cdc0006e5
Gabriel Eduardo de Souza <bieleprogramador22@gmail.com>
2020-12-27 15:30:33 -0300

    Parte 2

--- App.js
+++ App.js
@@ -1,29 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import AppLoading from 'expo';
+import { AntDesign } from '@expo/vector-icons'; 
+import { useFonts, Lato_900Black } from '@expo-google-fonts/lato';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
 
 
 export default function App() {
 
   const image = require('./images/bg.jpg')
-
+  let [fontsLoaded] = useFonts({
+    Lato_900Black,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
+      <StatusBar hidden/>
       <ImageBackground source={image} style={styles.image}>
         <View style={styles.coverView}>
           <Text style={styles.textHeader}>LISTA DE TAREFAS</Text>
         </View> 
       </ImageBackground>
-    </View>
-  );
-}
+
+      <View style={styles.tarefaSingle}>
+        <View style={{flex:1, width:'100%', padding:10}}>
+          <Text>Minha Tarefa do dia 1</Text>
+        </View>
+        <View style={{alignItems:'flex-end', flex:1, padding:10}}>
+          <TouchableOpacity><AntDesign name="minuscircleo" size={24} color="black" /></TouchableOpacity>
+        </View>
+      </View>
+      
+
+    </ScrollView>
+  );}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   image: {
-    flex: 1,
     resizeMode: 'cover',
     width:'100%',
     height:100,
@@ -37,6 +56,15 @@ const styles = StyleSheet.create({
     color:'white',
     fontSize:20,
     textAlign:'center',
-    marginTop:30
+    marginTop:30,
+    fontFamily:'Lato_900Black',
+  },
+  tarefaSingle:{
+    marginTop:30,
+    width:'100%',
+    borderBottomWidth:1,
+    borderBottomColor:'black',
+    flexDirection:'row',
+    paddingBottom:10
   }
 });
